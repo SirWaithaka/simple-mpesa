4d7a7d5511639bb3d6fda7a62234017326b687c6
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-31 01:11:56 +0300

    Add func handlers for all user types

---
+++ app/routing/user_handlers/admin_handlers.go
@@ -0,0 +1,74 @@
+package user_handlers
+
+import (
+	"net/http"
+
+	"simple-wallet/app"
+	"simple-wallet/app/admin"
+	"simple-wallet/app/auth"
+	"simple-wallet/app/models"
+	"simple-wallet/app/routing/responses"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthenticateAdmin(adminDomain admin.Interactor, config app.Config) fiber.Handler {
+
+	return func(ctx *fiber.Ctx) error {
+		var params admin.LoginParams
+		_ = ctx.BodyParser(&params)
+
+		err := params.Validate()
+		if err != nil {
+			return ctx.Status(http.StatusBadRequest).JSON(err)
+		}
+
+		// authenticate by email.
+		adm, err := adminDomain.AuthenticateByEmail(params.Email, params.Password)
+
+		// if there is an error authenticating admin.
+		if err != nil {
+			return err
+		}
+
+		// generate an auth token string
+		token, err := auth.GetTokenString(adm.ID, config.Secret)
+		if err != nil {
+			return err
+		}
+
+		signedUser := models.SignedUser{
+			UserID:   adm.ID.String(),
+			UserType: models.UserTypAdmin,
+			Token:    token,
+		}
+		_ = ctx.Status(http.StatusOK).JSON(signedUser)
+
+		return nil
+	}
+}
+
+func RegisterAdmin(adminDomain admin.Interactor) fiber.Handler {
+
+	return func(ctx *fiber.Ctx) error {
+
+		var params admin.RegistrationParams
+		_ = ctx.BodyParser(&params)
+
+		err := params.Validate()
+		if err != nil {
+			return ctx.Status(http.StatusBadRequest).JSON(err)
+		}
+
+		// register admin
+		adm, err := adminDomain.Register(params)
+		if err != nil {
+			return err
+		}
+
+		// we use a presenter to reformat the response of admin.
+		_ = ctx.Status(http.StatusOK).JSON(responses.RegistrationResponse(adm.ID, models.UserTypAdmin))
+
+		return nil
+	}
+}
---
+++ app/routing/user_handlers/subscriber_handlers.go
@@ -0,0 +1,74 @@
+package user_handlers
+
+import (
+	"net/http"
+
+	"simple-wallet/app"
+	"simple-wallet/app/auth"
+	"simple-wallet/app/models"
+	"simple-wallet/app/routing/responses"
+	"simple-wallet/app/subscriber"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthenticateSubscriber(subDomain subscriber.Interactor, config app.Config) fiber.Handler {
+
+	return func(ctx *fiber.Ctx) error {
+		var params subscriber.LoginParams
+		_ = ctx.BodyParser(&params)
+
+		err := params.Validate()
+		if err != nil {
+			return ctx.Status(http.StatusBadRequest).JSON(err)
+		}
+
+		// authenticate by email.
+		sub, err := subDomain.AuthenticateByEmail(params.Email, params.Password)
+
+		// if there is an error authenticating subscriber.
+		if err != nil {
+			return err
+		}
+
+		// generate an auth token string
+		token, err := auth.GetTokenString(sub.ID, config.Secret)
+		if err != nil {
+			return err
+		}
+
+		signedUser := models.SignedUser{
+			UserID:   sub.ID.String(),
+			UserType: models.UserTypeSubscriber,
+			Token:    token,
+		}
+		_ = ctx.Status(http.StatusOK).JSON(signedUser)
+
+		return nil
+	}
+}
+
+func RegisterSubscriber(subDomain subscriber.Interactor) fiber.Handler {
+
+	return func(ctx *fiber.Ctx) error {
+
+		var params subscriber.RegistrationParams
+		_ = ctx.BodyParser(&params)
+
+		err := params.Validate()
+		if err != nil {
+			return ctx.Status(http.StatusBadRequest).JSON(err)
+		}
+
+		// register subscriber
+		sub, err := subDomain.Register(params)
+		if err != nil {
+			return err
+		}
+
+		// we use a presenter to reformat the response of subscriber.
+		_ = ctx.Status(http.StatusOK).JSON(responses.RegistrationResponse(sub.ID, models.UserTypAdmin))
+
+		return nil
+	}
+}
