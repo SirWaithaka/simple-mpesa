f310f1ac77debb83868c053ccedc4634a76958b5
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-31 01:12:24 +0300

    Add login param definitions for subscriber

--- app/subscriber/params.go
+++ app/subscriber/params.go
@@ -4,7 +4,38 @@ import (
 	"simple-wallet/app/errors"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
+
+// LoginParams are properties required during login of a subscriber
+// email and phoneNumber can be interchanged, either is required
+type LoginParams struct {
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	PhoneNumber string `json:"phoneNumber"`
+}
+
+func (req LoginParams) Validate() error {
+
+	err := validation.ValidateStruct(&req,
+		validation.Field(&req.Password, validation.Required.Error(string(errors.ErrorPasswordRequired))),
+		// when validating email, if no phone number is provided, make sure email is valid and not empty
+		validation.Field(&req.Email,
+			validation.When(req.PhoneNumber == "", validation.Required.Error(string(errors.ErrorEmailRequired)), is.Email),
+		),
+		// when validating phone number, if no email is provided, make sure phone number is not empty
+		validation.Field(&req.PhoneNumber,
+			validation.When(req.Email == "", validation.Required.Error(string(errors.ErrorPhoneNumberRequired))),
+		),
+	)
+
+	e := errors.ParseValidationErrorMap(err)
+	if len(e) > 0 {
+		// we will return only the first error
+		return e[0]
+	}
+	return nil
+}
 
 // RegistrationParams are properties required during registration of a new subscriber
 type RegistrationParams struct {
