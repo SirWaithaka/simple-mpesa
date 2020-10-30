47d094e267d3113434b459c6c2312ec13f08f67a
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-31 01:42:37 +0300

    Inject additional domains

--- app/registry/domain.go
+++ app/registry/domain.go
@@ -3,25 +3,39 @@ package registry
 import (
 	"simple-wallet/app"
 	"simple-wallet/app/account"
+	"simple-wallet/app/admin"
+	"simple-wallet/app/agent"
+	"simple-wallet/app/merchant"
 	"simple-wallet/app/storage"
+	"simple-wallet/app/subscriber"
 	"simple-wallet/app/transaction"
-	"simple-wallet/app/user"
 )
 
 type Domain struct {
-	Account account.Interactor
+	Admin      admin.Interactor
+	Agent      agent.Interactor
+	Merchant   merchant.Interactor
+	Subscriber subscriber.Interactor
+
+	Account     account.Interactor
 	Transaction transaction.Interactor
-	User user.Interactor
 }
 
 func NewDomain(config app.Config, database *storage.Database, channels *Channels) *Domain {
+	adminRepo := admin.NewRepository(database)
+	agentRepo := agent.NewRepository(database)
+	merchantRepo := merchant.NewRepository(database)
+	subscriberRepo := subscriber.NewRepository(database)
+
 	accRepo := account.NewRepository(database)
-	userRepo := user.NewRepository(database)
 	txnRepo := transaction.NewRepository(database)
 
 	return &Domain{
-		Account: account.NewInteractor(accRepo, channels.ChannelNewUsers, channels.ChannelNewTransactions),
+		Admin:       admin.NewInteractor(config, adminRepo),
+		Agent:       agent.NewInteractor(config, agentRepo, channels.ChannelNewUsers),
+		Merchant:    merchant.NewInteractor(config, merchantRepo, channels.ChannelNewUsers),
+		Subscriber:  subscriber.NewInteractor(config, subscriberRepo, channels.ChannelNewUsers),
+		Account:     account.NewInteractor(accRepo, channels.ChannelNewUsers, channels.ChannelNewTransactions),
 		Transaction: transaction.NewInteractor(txnRepo, channels.ChannelNewTransactions),
-		User: user.NewInteractor(config, userRepo, channels.ChannelNewUsers),
 	}
 }
