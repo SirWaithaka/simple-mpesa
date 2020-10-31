ac9f95abb67eb0ca4686578aa4f5b897701b3a7c
Sir Waithaka <kennwaithaka@gmail.com>
2020-11-01 00:37:32 +0300

    Fix recursion bug on error code check; fix error message check on standard error

--- app/errors/errors.go
+++ app/errors/errors.go
@@ -48,23 +48,31 @@ func (e Error) Error() string {
 }
 
 // ErrorCode returns the code of the root error, if available. Otherwise returns EINTERNAL.
+//
+// 1. Returns no error code for nil errors.
+// 2. Searches the chain of Error.Err until a defined Code is found.
+// 3. If no code is defined then return an internal error code (EINTERNAL).
 func ErrorCode(err error) ERCode {
 	if err == nil {
 		return ""
 	} else if e, ok := err.(Error); ok && e.Code != "" {
 		return e.Code
 	} else if ok && e.Err != nil {
-		return ErrorCode(err)
+		return ErrorCode(e.Err)
 	}
 	return EINTERNAL
 }
 
 // ErrorMessage returns the human-readable message of the error, if available.
 // Otherwise returns a generic error message.
+//
+// 1. Returns no error message for nil errors.
+// 2. Searches the chain of Error.Err until a defined Message is found.
+// 3. If no message is defined then return a generic error message.
 func ErrorMessage(err error) string {
 	if err == nil {
 		return ""
-	} else if e, ok := err.(*Error); ok && e.Message != "" {
+	} else if e, ok := err.(Error); ok && e.Message != "" {
 		return string(e.Message)
 	} else if ok && e.Err != nil {
 		return ErrorMessage(e.Err)
