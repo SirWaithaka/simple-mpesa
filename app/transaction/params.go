f63a473271c1a59f0b6e21aed7a3abde5296a9d6
Sir Waithaka <kennwaithaka@gmail.com>
2020-11-13 17:15:54 +0300

    Add form and schema tags to param structs

--- app/transaction/params.go
+++ app/transaction/params.go
@@ -1,8 +1,6 @@
 package transaction
 
 import (
-	"log"
-
 	"simple-mpesa/app/errors"
 	"simple-mpesa/app/models"
 
@@ -10,11 +8,11 @@ import (
 )
 
 type DepositParams struct {
-	Amount models.Shillings `json:"amount"`
+	Amount models.Shillings `json:"amount" schema:"amount" form:"amount"`
 	// In a production system, the agent number is usually
 	// a generated number, but we are going to use the agent's
 	// email as a replacement
-	AgentNumber string `json:"agentNumber"`
+	AgentNumber string `json:"agentNumber" schema:"agentNumber" form:"agentNumber"`
 }
 
 func (req DepositParams) Validate() error {
@@ -24,19 +22,17 @@ func (req DepositParams) Validate() error {
 		validation.Field(&req.AgentNumber, validation.Required.Error(string(errors.ErrorAgentNumberRequired))),
 	)
 
-	e := errors.ParseValidationErrorMap(err)
-	log.Println(e)
-
-	return e
+	return errors.ParseValidationErrorMap(err)
 }
 
 type TransferParams struct {
-	Amount models.Shillings `json:"amount"`
+	Amount models.Shillings `json:"amount" schema:"amount" form:"amount"`
 
 	// In a production system, the account number is usually
-	// a generated number, but we are going to use the agent's
+	// a generated number, but we are going to use the customer's
 	// email as a replacement
-	DestAccountNo string `json:"destinationAccNo"`
+	DestAccountNo string          `json:"destinationAccNo" schema:"destinationAccNo" form:"destinationAccNo"`
+	DestUserType  models.UserType `json:"destinationUserType" schema:"destinationUserType" form:"destinationUserType"`
 }
 
 func (req TransferParams) Validate() error {
@@ -44,17 +40,18 @@ func (req TransferParams) Validate() error {
 	err := validation.ValidateStruct(&req,
 		validation.Field(&req.Amount, validation.Required.Error(string(errors.ErrorAmountRequired))),
 		validation.Field(&req.DestAccountNo, validation.Required.Error(string(errors.ErrorDestAccNumberRequired))),
+		validation.Field(&req.DestUserType, validation.Required.Error(string(errors.ErrorDestUserTypeRequired))),
 	)
 
 	return errors.ParseValidationErrorMap(err)
 }
 
 type WithdrawParams struct {
-	Amount models.Shillings `json:"amount"`
+	Amount models.Shillings `json:"amount" schema:"amount" form:"amount"`
 	// In a production system, the agent number is usually
 	// a generated number, but we are going to use the agent's
 	// email as a replacement
-	AgentNumber string `json:"agentNumber"`
+	AgentNumber string `json:"agentNumber" schema:"agentNumber" form:"agentNumber"`
 }
 
 func (req WithdrawParams) Validate() error {
