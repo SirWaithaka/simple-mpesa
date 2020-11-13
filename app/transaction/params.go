410bdbe48f1127317b07d5b8c4a4e09e48d8f47f
Sir Waithaka <kennwaithaka@gmail.com>
2020-11-14 00:31:12 +0300

    Update validation error messaging; Update param tags

--- app/transaction/params.go
+++ app/transaction/params.go
@@ -9,17 +9,19 @@ import (
 
 type DepositParams struct {
 	Amount models.Shillings `json:"amount" schema:"amount" form:"amount"`
-	// In a production system, the agent number is usually
-	// a generated number, but we are going to use the agent's
-	// email as a replacement
-	AgentNumber string `json:"agentNumber" schema:"agentNumber" form:"agentNumber"`
+	// In a production system, the customer number is usually a generated number for the case
+	// of a merchant of agent and a mobile number for a subscriber , but we are going to use the
+	// customer's email as a replacement
+	CustomerNumber string          `json:"accountNo" schema:"accountNo" form:"accountNo"`
+	CustomerType   models.UserType `json:"customerType" schema:"customerType" form:"customerType"`
 }
 
 func (req DepositParams) Validate() error {
 
 	err := validation.ValidateStruct(&req,
 		validation.Field(&req.Amount, validation.Required.Error(string(errors.ErrorAmountRequired))),
-		validation.Field(&req.AgentNumber, validation.Required.Error(string(errors.ErrorAgentNumberRequired))),
+		validation.Field(&req.CustomerNumber, validation.Required.Error(string(errors.ErrorAccountNumberRequired))),
+		validation.Field(&req.CustomerType, validation.Required.Error(string(errors.ErrorCustomerTypeRequired))),
 	)
 
 	return errors.ParseValidationErrorMap(err)
@@ -31,16 +33,16 @@ type TransferParams struct {
 	// In a production system, the account number is usually
 	// a generated number, but we are going to use the customer's
 	// email as a replacement
-	DestAccountNo string          `json:"destinationAccNo" schema:"destinationAccNo" form:"destinationAccNo"`
-	DestUserType  models.UserType `json:"destinationUserType" schema:"destinationUserType" form:"destinationUserType"`
+	DestAccountNo string          `json:"accountNo" schema:"accountNo" form:"accountNo"`
+	DestUserType  models.UserType `json:"customerType" schema:"customerType" form:"customerType"`
 }
 
 func (req TransferParams) Validate() error {
 
 	err := validation.ValidateStruct(&req,
 		validation.Field(&req.Amount, validation.Required.Error(string(errors.ErrorAmountRequired))),
-		validation.Field(&req.DestAccountNo, validation.Required.Error(string(errors.ErrorDestAccNumberRequired))),
-		validation.Field(&req.DestUserType, validation.Required.Error(string(errors.ErrorDestUserTypeRequired))),
+		validation.Field(&req.DestAccountNo, validation.Required.Error(string(errors.ErrorAccountNumberRequired))),
+		validation.Field(&req.DestUserType, validation.Required.Error(string(errors.ErrorCustomerTypeRequired))),
 	)
 
 	return errors.ParseValidationErrorMap(err)
