d63c96746a50e8e43ba2c271d00d630840c05d8e
Sir Waithaka <kennwaithaka@gmail.com>
2020-11-15 00:44:17 +0300

    Add a unique index to 3 columns of tariff identity

--- app/tariff/tariff.go
+++ app/tariff/tariff.go
@@ -10,9 +10,9 @@ import (
 type Tariff struct {
 	ID uuid.UUID
 
-	Transaction         models.TxnOperation
-	SourceUserType      models.UserType
-	DestinationUserType models.UserType
+	Transaction         models.TxnOperation `gorm:"uniqueIndex:idx_unique_tx_identity"`
+	SourceUserType      models.UserType     `gorm:"uniqueIndex:idx_unique_tx_identity"`
+	DestinationUserType models.UserType     `gorm:"uniqueIndex:idx_unique_tx_identity"`
 	Charge              models.Cents
 
 	gorm.Model
@@ -22,3 +22,12 @@ func (t *Tariff) BeforeCreate(tx *gorm.DB) error {
 	t.ID, _ = uuid.NewV4()
 	return nil
 }
+
+// ValidTransaction defines a format to identify all allowable transactions between customers
+// It is an array of 2 user types since only 2 customers types are allowed in a valid transaction
+// Index 0 is the source while index 1 is the destination
+//
+// Example
+// 1. Validation{models.UserTypSubscriber, models.UserTypMerchant}
+// Describes a valid transaction (transfer) between a subscriber to a merchant
+type ValidTransaction [2]models.UserType
