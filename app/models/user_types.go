ded81e2c196694f0174a7a07e0b3950053195e41
Sir Waithaka <kennwaithaka@gmail.com>
2020-11-15 15:11:55 +0300

    Add super agent as a user type

--- app/models/user_types.go
+++ app/models/user_types.go
@@ -2,9 +2,17 @@ package models
 
 type UserType string
 
+// IsAgent returns true if the user type matches
+// UserTypAgent or UserTypSuperAgent
+func (typ UserType) IsAgent() bool {
+	return typ == UserTypAgent || typ == UserTypSuperAgent
+}
+
 const (
 	UserTypAdmin      = UserType("administrator")
 	UserTypAgent      = UserType("agent")
 	UserTypMerchant   = UserType("merchant")
 	UserTypSubscriber = UserType("subscriber")
+
+	UserTypSuperAgent = UserType("super_agent")
 )
