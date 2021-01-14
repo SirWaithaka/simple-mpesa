791f9ca2dcc940262c97433f63e67eea9adbcec6
SirWaithaka <kennwaithaka@gmail.com>
2021-01-15 02:34:06 +0300

    Use repository schema for auto migration

--- src/storage/postgres/migrations.go
+++ src/storage/postgres/migrations.go
@@ -3,27 +3,21 @@ package postgres
 import (
 	"log"
 
-	"simple-mpesa/src/account"
-	"simple-mpesa/src/admin"
-	"simple-mpesa/src/agent"
-	"simple-mpesa/src/merchant"
+	"simple-mpesa/src/repositories/schema"
 	"simple-mpesa/src/storage"
-	"simple-mpesa/src/subscriber"
-	"simple-mpesa/src/tariff"
-	"simple-mpesa/src/transaction"
 )
 
 // Migrate updates the db with new columns, and tables
 func Migrate(database *storage.Database) {
 	err := database.DB.AutoMigrate(
-		admin.Administrator{},
-		agent.Agent{},
-		merchant.Merchant{},
-		subscriber.Subscriber{},
-		account.Account{},
-		transaction.Statement{},
-		account.Statement{},
-		tariff.Charge{},
+		schema.Admin{},
+		schema.Agent{},
+		schema.Merchant{},
+		schema.Subscriber{},
+		schema.Account{},
+		schema.Statement{},
+		schema.Statement{},
+		schema.Charge{},
 	)
 
 	if err != nil {
