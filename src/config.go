d64d527751cef5a5cd3a422fa427cf0a3d168aad
SirWaithaka <kennwaithaka@gmail.com>
2021-07-04 15:19:20 +0300

    Update to use dsn for db config

--- src/config.go
+++ src/config.go
@@ -1,46 +1,23 @@
 package src
 
 import (
-	"fmt"
-
 	"simple-mpesa/src/configs"
 )
 
-type Database struct {
-	User     string
-	Password string
-	Host     string
-	Port     string
-	DBName   string
-}
-
-func (d Database) String(sslmode string) string {
-	return fmt.Sprintf(""+
-		"host=%s "+
-		"port=%s "+
-		"user=%s "+
-		"dbname=%s "+
-		"password=%s "+
-		"sslmode=%v"+
-		"", d.Host, d.Port, d.User, d.DBName, d.Password, sslmode)
-}
-
 type Config struct {
-	DB Database
+	Driver string
+	DSN    string
 
 	Secret string
+
+	HTTPPort string
 }
 
-func GetConfig(cfg configs.YamlConfig) Config {
+func GetAppConfig(cfg *configs.EnvVarConfig) Config {
 	return Config{
-		DB: Database{
-			User:     cfg.Database.User,
-			Password: cfg.Database.Password,
-			Host:     cfg.Database.Host,
-			Port:     cfg.Database.Port,
-			DBName:   cfg.Database.DBName,
-		},
-
-		Secret: cfg.AppSecret,
+		Driver:   cfg.DBDriver,
+		Secret:   cfg.SecretKey,
+		DSN:      cfg.DBDSN,
+		HTTPPort: cfg.PORT,
 	}
 }
